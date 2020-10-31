2d2cc3d1fe55652a384ffb42135923ea4b8a7a4a
IssB20 <islem_ouiddir@yahoo.fr>
2020-10-31 12:42:55 -0400

    no treat or trick

--- src/test/java/HalloweenTest.java
+++ src/test/java/HalloweenTest.java
@@ -1,5 +1,11 @@
 public class HalloweenTest {
 
-// I am about to give yoy candy today for halloween
+    // I am about to give yoy candy today for halloween
+    // Are we there yet??? Hello Hello
+    // Lets play a game
+    // its fun to drive so far
+
+    // there will be no treat and trick this year
+    // stay home and safe and wear mask outside
 
 }
